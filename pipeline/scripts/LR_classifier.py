bad4b3ad8cefbc795314f4b8ec8e99aa10c5ad37
Liu Zechu <liuzechu2013@gmail.com>
2021-03-20 18:35:47 +0800

    add handcrafted features

--- pipeline/scripts/LR_classifier.py
+++ pipeline/scripts/LR_classifier.py
@@ -22,24 +22,26 @@ def load_pickle(filename):
     
     return processed_X, retrieved_df['Label']
 
-def combine_pickles(file1, file2, file3):
+def combine_pickles(file1, file2, file3, file4):
     # df_y is the same 
     df_X1, df_y = load_pickle(file1)
     df_X2, df_y = load_pickle(file2)
     df_X3, df_y = load_pickle(file3)
+    df_X4, df_y = load_pickle(file4)
     
     new_X = []
     for i in range(len(df_y)):
-        new_X.append(df_X1[i] + df_X2[i] + df_X3[i])
+        new_X.append(df_X1[i] + df_X2[i] + df_X3[i] + df_X4[i])
     
     return new_X, df_y
 
 def main():
     # Load data
-    # df_X, df_y = load_pickle(filename="../pickles/text_embeddings.pkl")
+    # df_X, df_y = load_pickle(filename="../pickles/handcrafted_features.pkl")
     df_X, df_y = combine_pickles(file1="../pickles/text_embeddings.pkl", 
         file2="../pickles/title_embeddings.pkl", 
-        file3="../pickles/word_count_vectors.pkl")
+        file3="../pickles/word_count_vectors.pkl",
+        file4="../pickles/handcrafted_features.pkl")
     
     #### analysis #####
     # print(pd.Series(df_y).value_counts())
@@ -75,7 +77,8 @@ def main():
     # Use accurracy as the metric
     y_test = y_test.astype('int')
     score = accuracy_score(y_test, y_pred)
-    print('accurracy score on validation = {}'.format(score))
+    print('Accurracy score on test set = {}'.format(score))
+
 
 if __name__ == "__main__":
     main()
