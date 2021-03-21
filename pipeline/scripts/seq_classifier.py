bdca487bcfc4a73d36ba1dce27af0bd5199f860c
Larry Law <larrylaw97@gmail.com>
2021-03-21 23:08:10 +0800

    Add seq classifier

--- pipeline/scripts/seq_classifier.py
+++ pipeline/scripts/seq_classifier.py
@@ -1,76 +1,132 @@
 #!/usr/bin/env python.
+import math
 import os
 import pickle
+
+import numpy as np
+import torch
 from dotenv import load_dotenv
-# import math
-# import numpy as np
-# import pandas as pd
-# from sklearn.metrics import f1_score
-# from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score
+from transformers import DistilBertTokenizerFast, DistilBertForSequenceClassification, Trainer, TrainingArguments
 
 load_dotenv()
 ROOT = os.environ.get("ROOT")
+
 SEQUENCE_FEATURES_FILE = f"{ROOT}/pipeline/pickles/sequence_features.pkl"
+SAVE_DIR = f"{ROOT}/results/seq_classifier"
+DEVICE = torch.device("cpu")  # "cuda"
+
+HP = {  # hyperparameters
+    "train_test_split": 0.8,
+    "num_train_epochs": 3,
+    "per_device_train_batch_size": 16,
+    "per_device_eval_batch_size": 64,
+    "warmup_steps": 500,
+    "weight_decay": 0.01,
+    "SEED": 1,
+    "logging_steps": 10,
+}
+
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
 
 
 def load_pickle(filename):
-    with (open(SEQUENCE_FEATURES_FILE, "rb")) as file:
+    with (open(filename, "rb")) as file:
         data = pickle.load(file)
-    print(data)
-    exit(1)
+    return data
 
 
-# pass in an array of filepaths as strings
-def combine_pickles(files):
-    pass
+def compute_metrics(pred):
+    ''' Argument for Trainer. '''
+    acc = accuracy_score(pred.label_ids, pred.predictions)
+    return {
+        'accuracy': acc,
+    }
 
 
 def main():
+    print("Preparing data...")
+    # Setting seeds to control randomness
+    np.random.seed(HP["SEED"])
+    torch.manual_seed(HP["SEED"])
+
     # Load data
-    load_pickle(SEQUENCE_FEATURES_FILE)
-    pass
-    # Load data
-    # df_X, df_y = load_pickle(filename="../pickles/handcrafted_features.pkl")
-    # df_X, df_y = combine_pickles([TEXT_EMBEDDINGS_FILE, TITLE_EMBEDDINGS_FILE, WORD_COUNT_VECTORS_FILE])
-    #
-    # # Train-Test split
-    # # [NOTE: No need to randomise as randomisation has already been done in scripts/dataframe_generator.py]
-    # training_length = math.ceil(len(df_X) * 0.8)
-    # X_train = df_X[:training_length]
-    # y_train = df_y[:training_length]
-    # X_test = df_X[training_length:]
-    # y_test = df_y[training_length:]
-    #
-    # # convert to numpy array to fit into the NN
-    # X_train = np.asarray(X_train)
-    # y_train = np.asarray(y_train)
-    # X_test = np.asarray(X_test)
-    # y_test = np.asarray(y_test)
-    #
-    # # Training
-    # model = tf.keras.models.Sequential([
-    #     tf.keras.layers.InputLayer(input_shape=(774,)),
-    #     tf.keras.layers.Dense(128, activation='relu'),
-    #     # tf.keras.layers.Dropout(0.2),
-    #     tf.keras.layers.Dense(32, activation='relu'),
-    #     tf.keras.layers.Dense(3, activation='softmax')
-    # ])
-    #
-    # model.compile(optimizer='adam',
-    #               loss='sparse_categorical_crossentropy',
-    #               metrics=['accuracy'])
-    # y_train = y_train.astype('int')
-    # print("X_train length: ", len(X_train))
-    # # print("10 examples of X_train: ", X_train[:10])
-    # print("X_train feature length: ", len(X_train[0]))
-    # print("y_train length: ", len(y_train))
-    # print("Training model now...")
-    # model.fit(X_train, y_train, epochs=5)
-    #
-    # # Testing
-    # print("Testing model now...")
-    # y_test = y_test.astype('int')
-    # model.evaluate(X_test, y_test)
+    data = load_pickle(SEQUENCE_FEATURES_FILE)
+
+    # X-Y split
+    X = []
+    Y = []
+    for x in data:
+        title, _, label = x  # TODO: try out body_text too
+        X.append(title)
+        Y.append(label)
+
+    # Train-Test split
+    # [NOTE: No need to randomise as randomisation has already been done in scripts/dataframe_generator.py]
+    training_length = math.ceil(len(X) * HP["train_test_split"])
+    X_train = X[:training_length]
+    Y_train = Y[:training_length]
+    X_test = X[training_length:]
+    Y_test = Y[training_length:]
+
+    # Preparing model
+    print("Preparing model...")
+    tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+    train_encodings = tokenizer(X_train, truncation=True, padding=True)
+    test_encodings = tokenizer(X_test, truncation=True, padding=True)
+
+    train_dataset = Dataset(train_encodings, Y_train)
+    test_dataset = Dataset(test_encodings, Y_test)
+
+    # Training
+    print("Training...")
+
+    training_args = TrainingArguments(
+        output_dir=SAVE_DIR,  # output directory
+        num_train_epochs=HP["num_train_epochs"],  # total number of training epochs
+        per_device_train_batch_size=HP["per_device_train_batch_size"],  # batch size per device during training
+        per_device_eval_batch_size=HP["per_device_eval_batch_size"],  # batch size for evaluation
+        warmup_steps=HP["warmup_steps"],  # number of warmup steps for learning rate scheduler
+        weight_decay=HP["weight_decay"],  # strength of weight decay
+        logging_dir='./logs',  # directory for storing logs
+        logging_steps=HP["logging_steps"],
+    )
+
+    model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=3)
+
+    trainer = Trainer(
+        model=model,  # the instantiated 🤗 Transformers model to be trained
+        args=training_args,  # training arguments, defined above
+        train_dataset=train_dataset,  # training dataset
+        eval_dataset=test_dataset,  # evaluation dataset
+        compute_metrics=compute_metrics  # accuracy metric
+    )
+
+    trainer.train()
+
+    results = trainer.evaluate()
+    info = HP
+    info["results"] = results
+
+    # saving results and model
+    print("Saving the good stuff...")
+    trainer.save_model(SAVE_DIR)
+    tokenizer.save_pretrained(SAVE_DIR)
+    data_file = open(f'{SAVE_DIR}/data.txt', "w+")
+    data_file.write(info)
+    data_file.close()
 
 
 if __name__ == "__main__":
