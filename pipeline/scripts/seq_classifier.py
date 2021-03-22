378fd0389bcfe7b8b54c50362cfdbcb6b960f96c
Larry Law <larrylaw@u.nus.edu>
2021-03-22 10:37:35 +0800

    [WIP] Loading pretrained model for seq classifier.

--- pipeline/scripts/seq_classifier.py
+++ pipeline/scripts/seq_classifier.py
@@ -14,7 +14,8 @@ ROOT = os.environ.get("ROOT")
 
 SEQUENCE_FEATURES_FILE = f"{ROOT}/pipeline/pickles/sequence_features.pkl"
 SAVE_DIR = f"{ROOT}/results/seq_classifier"
-DEVICE = torch.device("cpu")  # "cuda"
+LOAD_PATH = f"{ROOT}/results/seq_classifier/checkpoint-8000/"  # load pre-trained model. If non empty, will load model instead of training from scratch.
+DEVICE = torch.device("cpu")  # "cpu/cuda"
 
 HP = {  # hyperparameters
     "train_test_split": 0.8,
@@ -55,6 +56,47 @@ def compute_metrics(pred):
         'accuracy': acc,
     }
 
+def train_model(train_dataset, test_dataset):
+    print("Training model...")
+    model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=3)
+
+    training_args = TrainingArguments(
+        output_dir=SAVE_DIR,  # output directory
+        num_train_epochs=HP["num_train_epochs"],  # total number of training epochs
+        per_device_train_batch_size=HP["per_device_train_batch_size"],  # batch size per device during training
+        per_device_eval_batch_size=HP["per_device_eval_batch_size"],  # batch size for evaluation
+        warmup_steps=HP["warmup_steps"],  # number of warmup steps for learning rate scheduler
+        weight_decay=HP["weight_decay"],  # strength of weight decay
+        logging_dir='./logs',  # directory for storing logs
+        logging_steps=HP["logging_steps"],
+    )
+
+    trainer = Trainer(
+        model=model,  # the instantiated 🤗 Transformers model to be trained
+        args=training_args,  # training arguments, defined above
+        train_dataset=train_dataset,  # training dataset
+        eval_dataset=test_dataset,  # evaluation dataset
+        compute_metrics=compute_metrics  # accuracy metric
+    )
+
+    trainer.train()
+
+    return trainer
+
+def load_model(load_path, test_dataset):
+    print("Loading model...")
+    model = DistilBertForSequenceClassification.from_pretrained(load_path, num_labels=3)
+    model.to(DEVICE)
+
+    trainer = Trainer(
+        model=model,  # the instantiated 🤗 Transformers model to be trained
+        args=None,  # already trained
+        train_dataset=None,  
+        eval_dataset=test_dataset,  # evaluation dataset
+        compute_metrics=compute_metrics  # accuracy metric
+    )
+
+    return trainer
 
 def main():
     print("Preparing data...")
@@ -90,39 +132,21 @@ def main():
     train_dataset = Dataset(train_encodings, Y_train)
     test_dataset = Dataset(test_encodings, Y_test)
 
-    # Training
-    print("Training...")
+    # Building model
+    load = bool(LOAD_PATH)
+    if load:
+        trainer = load_model(LOAD_PATH, test_dataset)
+    else:  # train from scratch
+        trainer = train_model(train_dataset, test_dataset)
 
-    training_args = TrainingArguments(
-        output_dir=SAVE_DIR,  # output directory
-        num_train_epochs=HP["num_train_epochs"],  # total number of training epochs
-        per_device_train_batch_size=HP["per_device_train_batch_size"],  # batch size per device during training
-        per_device_eval_batch_size=HP["per_device_eval_batch_size"],  # batch size for evaluation
-        warmup_steps=HP["warmup_steps"],  # number of warmup steps for learning rate scheduler
-        weight_decay=HP["weight_decay"],  # strength of weight decay
-        logging_dir='./logs',  # directory for storing logs
-        logging_steps=HP["logging_steps"],
-    )
-
-    model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=3)
-
-    trainer = Trainer(
-        model=model,  # the instantiated 🤗 Transformers model to be trained
-        args=training_args,  # training arguments, defined above
-        train_dataset=train_dataset,  # training dataset
-        eval_dataset=test_dataset,  # evaluation dataset
-        compute_metrics=compute_metrics  # accuracy metric
-    )
-
-    trainer.train()
-
+    print("Evaluating...")
     results = trainer.evaluate()
     info = HP
     info["results"] = results
 
     # saving results and model
     print("Saving the good stuff...")
-    trainer.save_model(SAVE_DIR)
+    trainer.save_pretrained(SAVE_DIR)
     tokenizer.save_pretrained(SAVE_DIR)
     data_file = open(f'{SAVE_DIR}/data.txt', "w+")
     data_file.write(info)
