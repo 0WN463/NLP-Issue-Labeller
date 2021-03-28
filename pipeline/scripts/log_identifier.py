963ac40d58a56bbfecd3a4f8b6e9390fc5da1941
Liu Zechu <liuzechu2013@gmail.com>
2021-03-28 20:18:58 +0800

    edit log_identifier

--- pipeline/scripts/log_identifier.py
+++ pipeline/scripts/log_identifier.py
@@ -6,6 +6,6 @@ import re
 def has_log(text):
     pattern = r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}:|\
         [Tt]raceback.*:|[Bb]acktrace.*:|[Ll]ogs?:|\d{2}:\d{2}:\d{2}|\
-        (INFO|info|FAIL|fail|WARN(ING)?|warn(ing)?|FATAL|fatal|DEBUG|debug|SYSTEM|system)\s*:'
+        (INFO|info|FAIL|fail|WARN(ING)?|warn(ing)?|FATAL|fatal|DEBUG|debug|SYSTEM|system|ERROR|error)\s*:'
     results = re.findall(pattern, text)
     return len(results) != 0
