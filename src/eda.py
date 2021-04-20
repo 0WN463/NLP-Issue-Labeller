7c0ae67fae9b0aa904ef9b2acb54ee92d5c4f198
Larry Law <larrylaw97@gmail.com>
2021-04-20 12:52:11 +0800

    Minor fix to eda.py

--- src/eda.py
+++ src/eda.py
@@ -7,18 +7,18 @@ load_dotenv()
 ROOT = os.environ.get("ROOT")
 
 labels = ['URLs', 'Code', 'Logs']
-train = [0.856, 0.460, 0.223]
-test = [0.800, 0.420, 0.198]
+seen = [0.856, 0.460, 0.223]
+unseen = [0.800, 0.420, 0.198]
 
 x = np.arange(len(labels))  # the label locations
 width = 0.35  # the width of the bars
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(x - width/2, train, width, label='train')
-rects2 = ax.bar(x + width/2, test, width, label='test')
+rects1 = ax.bar(x - width/2, seen, width, label='seen repos')
+rects2 = ax.bar(x + width/2, unseen, width, label='unseen repos')
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_title('% of Machine information in train and test sets')
+ax.set_title('Proportion of Machine information in seen and unseen repos.')
 ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.legend()
